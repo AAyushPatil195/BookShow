25abf6c414a4c77212355c31ad88a76df9e449ef
AAyushPatil195 <aayushashokpatil@gmail.com>
2026-06-24 11:42:54 +0530

    fixed model and inngest function fixes

--- server/models/User.js
+++ server/models/User.js
@@ -7,4 +7,5 @@ const userSchema = new mongoose.Schema({
     image: {type: String, required: true} // URL
 }, {timestamp: true})
 
-export default User = mongoose.Model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
